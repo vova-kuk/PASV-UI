df126e18b3e80f1ee20094de360a012a6e9d0f7e
vova-kuk <58760099+vova-kuk@users.noreply.github.com>
2020-02-15 08:42:33 +0200

    delete old folder and create new Profile_Page (#4)

--- modules/user/profile_page/profilepage.spec.js
+++ modules/user/profile_page/profilepage.spec.js
@@ -2,7 +2,7 @@ import { expect } from 'chai';
 import ProfilePage from './ProfilePage';
 
 describe('PROFILEPAGE', () => {
-    it('should open the profilepage', () => {
+    it('should open the profile_page', () => {
         ProfilePage.open();
         browser.pause(5000)
     });
