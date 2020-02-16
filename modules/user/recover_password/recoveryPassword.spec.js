3293452d248390b792b68074fe4410108d051ec8
olgashchekina <olya6avg@gmail.com>
2020-02-16 01:04:54 -0800

    changed tests, deleted file

--- modules/user/recover_password/recoveryPassword.spec.js
+++ modules/user/recover_password/recoveryPassword.spec.js
@@ -1,16 +1,16 @@
-import { expect } from 'chai';
+import {expect} from 'chai';
 import HomePage from '../homepage/HomePage';
-import RecoveryPassword from "./RecoveryPassword";
+import LoginPage from "../login/LoginPage";
 
 describe('PASSWORD RECOVERY', () => {
-    it('should open the homepage', () => {
+    it('should open the Homepage', () => {
         HomePage.open();
     });
     it('should click Login button', () => {
-        RecoveryPassword.loginButton.click();
+        HomePage.loginButton.click();
     });
     it('should have correct User Login page title', () => {
-        expect(RecoveryPassword.h1.getText()).eq('User Login');
+        expect(LoginPage.h1.getText()).eq('User Login');
         browser.pause(1000);
     });
 
