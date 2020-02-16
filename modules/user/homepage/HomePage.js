884516141c91cb5238ae6da195295f0ca259a5b5
OlgaShchekina <57824176+OlgaShchekina@users.noreply.github.com>
2020-02-16 09:44:46 -0800

    Shchekina/recoveryPassword (#11)
    * changed tests, deleted file
    
    * changed test login-link

--- modules/user/homepage/HomePage.js
+++ modules/user/homepage/HomePage.js
@@ -1,14 +1,20 @@
 import AppPage from './../../AppPage';
 
-class HomePage extends AppPage{
-    get h1(){
+class HomePage extends AppPage {
+    get h1() {
         return $('//h1');
     }
+
     get title() {
         return $('//div[@class="container"]//span[@id="site-name"]');
     }
 
-    open(){
+    get loginLink() {
+        return $('//a[@qa="login-link"]')
+    }
+
+
+    open() {
         super.open('https://stage.pasv.us/')
     }
 }
--- modules/user/homepage/homepage.spec.js
+++ modules/user/homepage/homepage.spec.js
@@ -1,11 +1,11 @@
-import { expect } from 'chai';
+import {expect} from 'chai';
 import HomePage from './HomePage';
 
 describe('HOMEPAGE', () => {
-  it('should open the homepage', () => {
-    HomePage.open();
-  });
-  it('should have correct Home page title', () => {
-    expect(HomePage.title.getText()).eq('Progress Monitor');
-  });
+    it('should open the homepage', () => {
+        HomePage.open();
+    });
+    it('should have correct Home page title', () => {
+        expect(HomePage.title.getText()).eq('Progress Monitor');
+    });
 });
