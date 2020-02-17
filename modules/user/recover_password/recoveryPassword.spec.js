978ffb7164ed3dba8088d331e51dc1b70539dda5
Miradil Omuraliev <omiradil@gmail.com>
2020-02-16 20:32:25 -0800

    changed format of `it`

--- modules/user/recover_password/recoveryPassword.spec.js
+++ modules/user/recover_password/recoveryPassword.spec.js
@@ -36,11 +36,9 @@ describe('PASSWORD RECOVERY', () => {
     it('should check failed message `User not found` appears if entered email is not found in the database', () => {
         ResetPasswordPage.emailReset.setValue('asdadfdfs@mail.com');
         ResetPasswordPage.sendButton.click();
-        browser.pause(2000)
-        const actual = ResetPasswordPage.h4.getText();
-        const expected = 'User not found';
-        expect(actual).eq(expected);
-        browser.pause(2000)
+        browser.pause(1000)
+        expect(ResetPasswordPage.h4.getText()).eq('User not found');
+        browser.pause(1000)
     });
 
     it('should check that user is left on the same page if entered email is not found in the database', () => {
@@ -51,11 +49,9 @@ describe('PASSWORD RECOVERY', () => {
     it('should check that user gets redirected to `CheckMail` page if correct email was entered', () => {
         ResetPasswordPage.emailReset.setValue('ooopartner00@mail.ru');
         ResetPasswordPage.sendButton.click();
-        browser.pause(2000)
-        const actual = CheckEmailPage.h1Check.getText();
-        const expected = 'Check your email for a link to reset your password';
-        expect(actual).eq(expected);
-        browser.pause(2000)
+        browser.pause(1000)
+        expect(CheckEmailPage.h1Check.getText()).eq('Check your email for a link to reset your password') ;
+        browser.pause(1000)
     });
 
 });
