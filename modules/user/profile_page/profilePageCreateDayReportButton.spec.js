b934ffe892b1fb7c3154e129e7feb17f61997b04
Lyudmyla Tkachenko <luda_tkach@yahoo.com>
2020-02-16 23:28:56 -0800

    Added profile page test files

--- modules/user/profile_page/profilePageCreateDayReportButton.spec.js
+++ modules/user/profile_page/profilePageCreateDayReportButton.spec.js
@@ -2,14 +2,14 @@ import { expect } from 'chai';
 import ProfilePage from '../../_PageObjects/ProfilePage';
 import LoginPage from "../../_PageObjects/LoginPage";
 
-describe('PROFILEPAGE', () => {
+describe('PROFILE PAGE Create day report button', () => {
     before(() => {
         LoginPage.login();
     });
 
     it('should go to profile Page', () => {
         browser.pause(5000);
-        expect(ProfilePage.h1.getText()).eq('Test Testov')
+        expect(ProfilePage.h1.getText()).equal('Test Testov');
     });
 
     it('should choose click Create day report button', () => {
@@ -17,4 +17,11 @@ describe('PROFILEPAGE', () => {
         element.click();
         browser.pause(5000);
     });
+
+    //change to Create day report Page selector
+    it('shoudl have header Create day report', () => {
+        expect(ProfilePage.h1.getText()).equal('Create day report');
+    });
 });
+
+
