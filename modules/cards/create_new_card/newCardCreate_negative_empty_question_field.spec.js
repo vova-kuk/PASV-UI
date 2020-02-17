0e047b7dddd8f4ea70adfccdfbb1fe3364a8c74b
Liudmila Pe <a553311@yahoo.ca>
2020-02-17 15:50:05 -0500

    Function loginRole(role) added to LoginPage.js. File loginRole_data.js added to login block. Tests for NewCardCreate rewitten with new loginRole function.

--- modules/cards/create_new_card/newCardCreate_negative_empty_question_field.spec.js
+++ modules/cards/create_new_card/newCardCreate_negative_empty_question_field.spec.js
@@ -2,11 +2,11 @@ import {expect} from 'chai';
 import FlashCardsPage from '../../_PageObjects/FlashCardsPage';
 import LoginPage from '../../_PageObjects/LoginPage';
 import {negative1} from './newCard_data';
+import {student} from '../../user/login/loginRole_data';
 
 describe('NEW FLASH CARD CREATE WITH EMPTY QUESTION', ()=> {
     before('should login as student', () => {
-// ====== currently logging in as an admin. Waiting for login(role) implementation ==========
-        LoginPage.login();
+        LoginPage.loginRole(student);
     });
 
     it('should open Flash Cards page', () => {
