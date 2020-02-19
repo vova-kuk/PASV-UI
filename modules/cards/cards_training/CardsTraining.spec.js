1918aa49a25a9bb4a000f39f7b5fc43673dc8a76
Anna Strik <58706349+annastrik@users.noreply.github.com>
2020-02-18 22:45:31 -0800

    Cards training anna strik (#46)
    * Cards training, my part
    
    * Cards training, my part

--- modules/cards/cards_training/CardsTraining.spec.js
+++ modules/cards/cards_training/CardsTraining.spec.js
@@ -1,9 +1,42 @@
 import {expect} from 'chai';
 import CardsTrainingPage from "../../_PageObjects/CardsTrainingPage";
 import LoginPage from "../../_PageObjects/LoginPage";
+import CheckEmailPage from "../../_PageObjects/CheckEmailPage";
+
+let progressBefore;
 
 describe('', () => {
     before('login', () => {
 
+    });
+
+    it('should check if answer is displayed', () => {
+        expect(CardsTrainingPage.answer.isDisplayed()).true;
+    });
+
+    it('should click `Get random next`', () => {
+        CardsTrainingPage.getRandomClick();
+        browser.pause(500);
+    });
+    it('should click `Get random next`', () => {
+        CardsTrainingPage.getRandomClick();
+        browser.pause(500);
+    });
+
+    it('should check progress bar before `I Know` click', () => {
+        progressBefore = CardsTrainingPage.progress.getText();
+        browser.pause(2000);
+    });
+
+    it('should click `I Know`', () => {
+        CardsTrainingPage.iKnowClick();
+        browser.pause(500);
+    });
+
+    it('should check if progress has changed', () => {
+        browser.refresh();
+        const progressAfter = CardsTrainingPage.progress.getText();
+        expect(progressAfter).to.not.equal(progressBefore);
+        browser.pause(1000);
     });
 });
