faa1b35dce736a5885025dbfc52033f79eb8d12a
Dmitriy Kovalenko <qa_19@outlook.com>
2020-02-18 19:19:46 -0800

    Change from open Registration page URL to Home Page - click registration link - Go to Registration Page

--- modules/user/registration/registration_negativeEmptyFirstName.spec.js
+++ modules/user/registration/registration_negativeEmptyFirstName.spec.js
@@ -1,63 +1,66 @@
 import {expect} from 'chai';
 import RegistrationPage from '../../_PageObjects/RegistrationPage';
 import {newUser, pageRegister} from './registration_data';
+import HomePage from "../../_PageObjects/HomePage";
 
 describe('REGISTRATION PAGE NEGATIVE with empty field `First Name`', () => {
-    it('should open registration page', () => {
-        RegistrationPage.open();
-        browser.pause(1000);
-    });
+  before('should open Home Page, click button `Registration` ' +
+    'and redirect to Registration Page',() => {
+    HomePage.open();
+    HomePage.registrationLink.click();
+    browser.pause(1000);
+  });
 
-    it('should have correct title', () => {
-        const actual = RegistrationPage.title.getText();
-        const expected = pageRegister.title;
-        expect(actual).eq(expected);
-    });
+  it('should registration page has correct title', () => {
+    const actual = RegistrationPage.title.getText();
+    const expected = pageRegister.title;
+    expect(actual).eq(expected);
+  });
 
-    it('should have correct heading', () => {
-        const actual = RegistrationPage.h1.getText();
-        const expected = pageRegister.h1;
-        expect(actual).eq(expected);
-    });
+  it('should registration page has correct heading', () => {
+    const actual = RegistrationPage.h1.getText();
+    const expected = pageRegister.h1;
+    expect(actual).eq(expected);
+  });
 
-    it('should have correct description', () => {
-        const actual = RegistrationPage.dummyText.getText();
-        const expected = pageRegister.dummyText;
-        expect(actual).eq(expected);
-    });
+  it('should have correct description', () => {
+    const actual = RegistrationPage.dummyText.getText();
+    const expected = pageRegister.dummyText;
+    expect(actual).eq(expected);
+  });
 
-    it('should fill out Last Name Field', () => {
-        RegistrationPage.lastName.setValue(newUser.lastName);
-    });
+  it('should fill out Last Name Field', () => {
+    RegistrationPage.lastName.setValue(newUser.lastName);
+  });
 
-    it('should fill out Cell Phone Number Field', () => {
-        RegistrationPage.cellPhoneNumber.setValue(newUser.phone);
-    });
+  it('should fill out Cell Phone Number Field', () => {
+    RegistrationPage.cellPhoneNumber.setValue(newUser.phone);
+  });
 
-    it('should fill out Email field', () => {
-        RegistrationPage.email.setValue(newUser.email);
-    });
+  it('should fill out Email field', () => {
+    RegistrationPage.email.setValue(newUser.email);
+  });
 
-    it('should fill out Password field', () => {
-        RegistrationPage.password.setValue(newUser.password);
-    });
+  it('should fill out Password field', () => {
+    RegistrationPage.password.setValue(newUser.password);
+  });
 
-    it('should fill out About field', () => {
-        RegistrationPage.about.setValue(newUser.about);
-    });
+  it('should fill out About field', () => {
+    RegistrationPage.about.setValue(newUser.about);
+  });
 
-    it('should fill out My goals field', () => {
-        RegistrationPage.myGoals.setValue(newUser.goals);
-    });
+  it('should fill out My goals field', () => {
+    RegistrationPage.myGoals.setValue(newUser.goals);
+  });
 
-    it('should choose English level from dropdown menu', () => {
-        RegistrationPage.englishLevel.selectByVisibleText(newUser.englishLevel);
-    });
+  it('should choose English level from dropdown menu', () => {
+    RegistrationPage.englishLevel.selectByVisibleText(newUser.englishLevel);
+  });
 
-    it('should button `Submit` will be Disable with empty First Name field', () => {
-        const element = RegistrationPage.submitBtn;
-        expect(element.isEnabled()).to.be.false;
-    });
+  it('should button `Submit` will be Disable with empty First Name field', () => {
+    const element = RegistrationPage.submitBtn;
+    expect(element.isEnabled()).to.be.false;
+  });
 
 });
 
