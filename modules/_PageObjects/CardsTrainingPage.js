75cde64120aae6c9b4fe78f406191c7ab6dc6ddb
Anzhelika Kusevych <lika.kusevich@gmail.com>
2020-02-19 02:38:32 -0500

    Cards training tests, first tests

--- modules/_PageObjects/CardsTrainingPage.js
+++ modules/_PageObjects/CardsTrainingPage.js
@@ -1,16 +1,35 @@
 import AppPage from "../AppPage";
 class CardsTrainingPage extends AppPage{
 
-    get startTraining (){
+    get startTraining () {
         return $('//button[contains(text(),"Start training")]');
     }
-    get iKnow(){
+    get iKnow() {
         return $('//button[contains(text(),"I know")]');
     }
 
+    get getRandom() {
+        return $('//button[contains(text(),"Get random next")]');
+    }
+
+    get answer() {
+        return $('//div[@class="result")]');
+    }
+
+    get progress() {
+        return $('//div[@class="progress")]//div[@aria-valuenow="0"]');
+    }
 
     open(path) {
         super.open('')
+    }
+
+    getRandomClick() {
+        this.getRandom.click();
+    }
+
+    iKnowClick() {
+        this.iKnow.click();
     }
 
 
--- modules/cards/cards_training/CardsTraining.spec.js
+++ modules/cards/cards_training/CardsTraining.spec.js
@@ -1,9 +1,58 @@
 import {expect} from 'chai';
+import ProfilePage from '../../_PageObjects/ProfilePage';
+import FlashCardsPage from "../../_PageObjects/FlashCardsPage";
+import {student} from "../../user/login/loginRole_data";
 import CardsTrainingPage from "../../_PageObjects/CardsTrainingPage";
 import LoginPage from "../../_PageObjects/LoginPage";
+import CheckEmailPage from "../../_PageObjects/CheckEmailPage";
+let progressBefore;
 
-describe('', () => {
-    before('login', () => {
-
+describe ('CARDS TRAINING', () => {
+    before('login as a student', () => {
+        LoginPage.loginRole(student);
     });
+
+    it ('should click main menu "Cards" link', () => {
+        ProfilePage.cardsLink.click();
+        browser.pause(500);
+    });
+
+    it('should have correct h1 title', () => {
+        expect(FlashCardsPage.h1.getText()).eq('FlashCards');
+    });
+
+//Anna
+    /*
+    it('should check if answer is displayed', () => {
+        expect(CardsTrainingPage.answer.isDisplayed()).true;
+    });
+
+    it('should click `Get random next`', () => {
+        CardsTrainingPage.getRandomClick();
+        browser.pause(500);
+    });
+
+    it('should click `Get random next`', () => {
+        CardsTrainingPage.getRandomClick();
+        browser.pause(500);
+    });
+
+    it('should check progress bar before `I Know` click', () => {
+        progressBefore = CardsTrainingPage.progress.getText();
+        browser.pause(2000);
+    });
+
+    it('should click `I Know`', () => {
+        CardsTrainingPage.iKnowClick();
+        browser.pause(500);
+    });
+
+    it('should check if progress has changed', () => {
+        browser.refresh();
+        const progressAfter = CardsTrainingPage.progress.getText();
+        expect(progressAfter).to.not.equal(progressBefore);
+        browser.pause(1000);
+    });
+
+     */
 });
