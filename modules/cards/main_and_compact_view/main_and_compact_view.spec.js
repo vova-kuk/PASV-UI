45b092dd24339197100170d85c2bc2c32ce6d44c
Ekaterina Avdeeva <katavdeeva@gmail.com>
2020-02-20 12:52:40 -0700

    Updated the describe and it.

--- modules/cards/main_and_compact_view/main_and_compact_view.spec.js
+++ modules/cards/main_and_compact_view/main_and_compact_view.spec.js
@@ -1,43 +1,44 @@
 import {expect} from 'chai';
 import LoginPage from '../../_PageObjects/LoginPage';
-import MainAndCompactViewPage from "../../_PageObjects/MainAndCompactViewPage";
-import FlashCardsPage from "../../_PageObjects/FlashCardsPage";
+import MainAndCompactViewPage from '../../_PageObjects/MainAndCompactViewPage';
+import FlashCardsPage from '../../_PageObjects/FlashCardsPage';
 import {student} from '../../user/login/loginRole_data';
 import {pageTestData} from './main_and_compact_view_data';
-import {groupNameLink} from "../create_new_card/newCard_data";
+import {groupNameLink} from '../create_new_card/newCard_data';
 
-describe('MAIN AND COMPACT VIEW', () => {
-    it('should log into user account', () => {
+describe('EXIST A MAIN AND COMPACT VIEW OF A FLASH CARD', () => {
+    before('login as a student', () => {
         LoginPage.loginRole(student);
+        browser.pause(500);
     });
 
-    it('should go to Cards page', () => {
+    it('should go to `FlashCards` page', () => {
         FlashCardsPage.open();
         browser.pause(2000);
         expect(pageTestData.title).equal(FlashCardsPage.h1.getText());
     });
 
-    it('should go to Group\'s page', () => {
+    it('should go to `FlashCards` group page', () => {
         browser.$(groupNameLink).click();
         browser.pause(2000);
         expect(pageTestData.groupName).equal(FlashCardsPage.h1.getText());
     });
 
-   it ('should redirect to Main view', () => {
+   it ('should redirect to `Main view`', () => {
        MainAndCompactViewPage.mainView.click();
        expect(MainAndCompactViewPage.mainViewActive.isExisting()).equal(true)
    });
 
-    it ('should redirect to Compact view', () => {
+    it ('should redirect to `Compact view`', () => {
         MainAndCompactViewPage.compactView.click();
         expect(MainAndCompactViewPage.compactViewActive.isExisting()).equal(true)
     });
 
-    it ('should check, that text does not appear, before hover over the question', () => {
+    it ('should check that the text does not appear before hovering over the question', () => {
         expect(MainAndCompactViewPage.answerText.isExisting()).equals(false);
     });
 
-    it ('should check, that text appears, when hover over the question', () => {
+    it ('should check that the text appears when hovering over the question', () => {
         MainAndCompactViewPage.questionText.moveTo();
         browser.pause(100);
         expect(MainAndCompactViewPage.answerText.isExisting()).equals(true);
