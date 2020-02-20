d93c40c0b513eb5de57328d01136b95e0845e63b
Ekaterina Avdeeva <katavdeeva@gmail.com>
2020-02-20 12:22:32 -0700

    Updated the describe and it.

--- modules/cards/create_new_card/newCardCreate_negative_empty_answer_field.spec.js
+++ modules/cards/create_new_card/newCardCreate_negative_empty_answer_field.spec.js
@@ -4,34 +4,34 @@ import LoginPage from '../../_PageObjects/LoginPage';
 import {negative2} from './newCard_data';
 import {student} from '../../user/login/loginRole_data';
 
-describe('NEW FLASH CARD CREATE WITH EMPTY ANSWER', () => {
-  before('should login as student', () => {
+describe('CREATE NEW CARD NEGATIVE with empty answer', () => {
+  before('should login as a student', () => {
     LoginPage.loginRole(student);
   });
 
-  it('should open Flash Cards page', () => {
+  it('should open `FlashCards` page', () => {
     FlashCardsPage.open();
   });
 
-  it('should check if the group exists', () => {
+  it('should check if the `FlashCards`group exists', () => {
     const actual = FlashCardsPage.groupName;
     expect(actual).to.exist;
   });
 
-  it('should click the link with Flash Group name', () => {
+  it('should click the link with `FlashCards` group name', () => {
     FlashCardsPage.groupName.click();
   });
 
-  it('should click Waiting for approval link', () => {
+  it('should click `Waiting for approval` link', () => {
     FlashCardsPage.waitingForApproval.click();
   });
 
-  it('should click Create new Card button', () => {
+  it('should click `Create new Card` button', () => {
     FlashCardsPage.createNewCardBtn.click();
     browser.pause(2000);
   });
 
-  it('should check if modal form is open', () => {
+  it('should check if  a modal form is opened', () => {
     const element = FlashCardsPage.modalForm;
     expect(element.isDisplayed()).true;
   });
@@ -42,16 +42,16 @@ describe('NEW FLASH CARD CREATE WITH EMPTY ANSWER', () => {
     expect(actual).equal(expected);
   });
 
-  it('should fill in the Question field', () => {
+  it('should fill in the `Question` text area', () => {
     FlashCardsPage.question.setValue(negative2.questionText);
   });
 
-  it('should fill in the Answer field', () => {
+  it('should fill in the `Answer` text area', () => {
     FlashCardsPage.answer.setValue(negative2.answerText);
     browser.pause(3000);
   });
 
-  it('should check if Create button is disabled', () => {
+  it('should check if `Create` button is disabled', () => {
     const element = FlashCardsPage.createBtn.isEnabled();
     expect(element).be.false;
   });
@@ -60,6 +60,6 @@ describe('NEW FLASH CARD CREATE WITH EMPTY ANSWER', () => {
     browser.$('//button[@class = "close"]').click();
     FlashCardsPage.logout();
     browser.pause(3000);
-  })
+  });
 
 });
