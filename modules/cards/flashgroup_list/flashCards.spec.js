d3cb8e2269ee79398d31460c20e01454687e5775
Ekaterina Avdeeva <katavdeeva@gmail.com>
2020-02-20 12:46:00 -0700

    Updated the describe and it.

--- modules/cards/flashgroup_list/flashCards.spec.js
+++ modules/cards/flashgroup_list/flashCards.spec.js
@@ -1,15 +1,15 @@
 import { expect } from 'chai';
-import FlashCardsPage from "../../_PageObjects/FlashCardsPage";
-import LoginPage from "../../_PageObjects/LoginPage";
-import {student} from "../../user/login/loginRole_data";
+import FlashCardsPage from '../../_PageObjects/FlashCardsPage';
+import LoginPage from '../../_PageObjects/LoginPage';
+import {student} from '../../user/login/loginRole_data';
 
-describe('(POSITIVE): FlashCards: Check Title and Description', () => {
-    before('Login as Student', () => {
+describe('FLASH CARD EXISTS POSITIVE with a title and description', () => {
+    before('login as a student', () => {
         LoginPage.loginRole(student);
         browser.pause(500);
     });
 
-    it('should check page title', () => {
+    it('should check a `FlashCards` page title', () => {
         FlashCardsPage.openCardsMenu().click();
         const actual = FlashCardsPage.h1.getText();
         const expected = 'FlashCards';
@@ -17,7 +17,7 @@ describe('(POSITIVE): FlashCards: Check Title and Description', () => {
         browser.pause(200);
     });
 
-    it('should check description', function () {
+    it('should check a description', function () {
         const actual = FlashCardsPage.description.getText();
         const expected = 'Now the stage of filling content cards. Create cards. After there will be enough cards training will be opened for memorization.';
         expect(actual).eq(expected);
@@ -44,18 +44,18 @@ describe('(POSITIVE): FlashCards: Check Title and Description', () => {
         expect(actual).eq(expected);
     });
 
-    it('should check `Main view` title of Navigation Menu link', function () {
+    it('should check `Main view` title in the navigation menu link', function () {
         FlashCardsPage.firstCreatedCard.click();
         const actual=FlashCardsPage.mainView.getText();
         expect(actual).eq('Main view');
     });
 
-    it('should check `Compact view` title of Navigation Menu link', function () {
+    it('should check `Compact view` title in the navigation menu', function () {
         const actual=FlashCardsPage.compactView.getText();
         expect(actual).eq('Compact view');
     });
 
-    it('should check `Waiting for approval` title of Navigation Menu link', function () {
+    it('should check `Waiting for approval` title in the navigation menu', function () {
         const actual=FlashCardsPage.waitingForApproval.getText();
         expect(actual).eq('Waiting for approval');
     });
@@ -65,27 +65,27 @@ describe('(POSITIVE): FlashCards: Check Title and Description', () => {
         expect(actual).eq('Training');
     });
 
-    it('should check link `Main view` be clickable/active', function () {
+    it('should check `Main view` link is clickable', function () {
         const actual=FlashCardsPage.mainView.isClickable();
         expect(actual).be.true;
     });
 
-    it('should check link `Compact view` be clickable/active', function () {
+    it('should check `Compact view` link is clickable', function () {
         const actual=FlashCardsPage.compactView.isClickable();
         expect(actual).be.true;
     });
 
-    it('should check link `Waiting for approval` be clickable/active', function () {
+    it('should check `Waiting for approval` link is clickable', function () {
         const actual=FlashCardsPage.waitingForApproval.isClickable();
         expect(actual).be.true;
     });
 
-    it('should check link `Training` be clickable/active', function () {
+    it('should check `Training` link is clickable', function () {
         const actual=FlashCardsPage.training.isClickable();
         expect(actual).be.true;
-    })
+    });
 
-})
+});
 
 
 
