d2eb6652e06ee4d1229ef33b7c7be4de97dce5be
Ekaterina Avdeeva <katavdeeva@gmail.com>
2020-02-20 19:33:42 -0700

    changed Logout to LogoutPage

--- modules/diary/create_day_report/createDayReport_negative.spec.js
+++ modules/diary/create_day_report/createDayReport_negative.spec.js
@@ -1,48 +1,61 @@
-import LoginPage from "../../_PageObjects/LoginPage";
-import Logout from "../../_PageObjects/Logout";
-import CreateDayReportPage from "../../_PageObjects/CreateDayReportPage";
+import LoginPage from '../../_PageObjects/LoginPage';
+import Logout from '../../_PageObjects/LogoutPage';
+import CreateDayReportPage from '../../_PageObjects/CreateDayReportPage';
 
-describe('CREATE DAY REPORT', () => {
-    before('Should login as STUDENT', () => {
+describe('CREATE DAY REPORT NEGATIVE', () => {
+    before('should login as a student', () => {
         LoginPage.loginAsStudent();
     });
-    it('Should click on Create Day Report button', () => {
+
+    it('should click on Create Day Report button', () => {
         CreateDayReportPage.createDayReport();
     });
-    it('Should check page Header', () => {
+
+    it('should check page Header', () => {
         expect(CreateDayReportPage.header.getText()).not.eq('ate day report');
     });
-    it('Should check page Marks', () => {
+
+    it('should check page Marks', () => {
         expect(CreateDayReportPage.marks.getText()).not.eq('rks to your daily report');
     });
-    it('Should check Slogan', () => {
+
+    it('should check Slogan', () => {
         expect(CreateDayReportPage.slogan.getText()).not.eq('2020 And what do you do for your career?');
     });
-    it('Should check Save button text', () => {
+
+    it('should check Save button text', () => {
         expect(CreateDayReportPage.saveButton.getText()).not.eq('save');
     });
-    it('Should fill out Day Report, setting incorrect values', () => {
+
+    it('should fill out Day Report, setting incorrect values', () => {
         CreateDayReportPage.fillOutReportNeg();
     });
-    it('Should display Marks to your daily report Required warning', () => {
+
+    it('should display Marks to your daily report Required warning', () => {
         CreateDayReportPage.marksRequired.isDisplayed();
     });
-    it('Should display What is your morale Required warning', () => {
+
+    it('should display What is your morale Required warning', () => {
         CreateDayReportPage.moraleRequired.isDisplayed();
     });
-    it('Should display Maximum 10 warning', () => {
+
+    it('should display Maximum 10 warning', () => {
         CreateDayReportPage.maxTen.isDisplayed();
     });
-    it('Should display Must be 30 characters or more warning', () => {
+
+    it('should display Must be 30 characters or more warning', () => {
         CreateDayReportPage.mustBeThirty.isDisplayed();
     });
-    it('Should check that Save button is not active', () => {
+
+    it('should check that Save button is not active', () => {
         expect(CreateDayReportPage.saveButton.isEnabled()).be.not.true;
     });
-    it('Should not be able to click Save button', () => {
+
+    it('should not be able to click Save button', () => {
         expect(CreateDayReportPage.saveButton.isClickable()).be.not.true;
     });
-    after('Should logout', () => {
+
+    after('should logout', () => {
         Logout.logout();
     });
 });
--- modules/user/logout/logout.spec.js
+++ modules/user/logout/logout.spec.js
@@ -1,6 +1,6 @@
 import LoginPage from '../../_PageObjects/LoginPage'
 import { student } from '../login/loginRole_data'
-import Logout from '../../_PageObjects/Logout'
+import Logout from '../../_PageObjects/LogoutPage'
 
 describe('LOGOUT', () => {
   before(() => {
