b7aaf7eda610a5cadee677f57b7cabd1669bbdc4
Elena <lenysik@Elenas-MacBook-Air.local>
2020-02-23 11:18:18 -0800

    Added test cases for group rating + selectors

--- modules/_PageObjects/GroupRating.js
+++ modules/_PageObjects/GroupRating.js
@@ -3,7 +3,19 @@ import AppPage from '../AppPage';
 class GroupRating extends AppPage {
 
     get linkToGroup() {
-        return browser.$('=Group4Test');
+        return $('=Group4Test');
+    }
+
+    get ratingLink() {
+        return $('=Rating');
+    }
+
+    get h1 () {
+        return $('//h1');
+    }
+
+    get ratingTable() {
+        return $('.rt-table');
     }
 
 }
