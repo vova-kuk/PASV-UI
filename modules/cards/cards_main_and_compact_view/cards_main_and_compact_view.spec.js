4b98fe6856ed92ed53210892a0b0653e483147b0
dipon <diponyelena@gmail.com>
2020-02-24 22:31:57 -0800

    1) Changed mainAndCompact.data.js to cardsMainAndCompact.data.js. 2) Changed main_and_compact_view.spec.js to cards_main_and_compact_view.spec.js. 3) Changed some titles in spec file and added 2 test cases.

--- modules/cards/cards_main_and_compact_view/cards_main_and_compact_view.spec.js
+++ modules/cards/cards_main_and_compact_view/cards_main_and_compact_view.spec.js
@@ -3,44 +3,49 @@ import LoginPage from '../../_PageObjects/LoginPage';
 import MainAndCompactViewPage from '../../_PageObjects/MainAndCompactViewPage';
 import FlashCardsPage from '../../_PageObjects/FlashCardsPage';
 import {student} from '../../_data/user.data';
-import {pageTestData} from '../../_data/mainAndCompact.data';
+import {pageTestData} from '../../_data/cardsMainAndCompact.data';
 import {groupNameLink} from '../../_data/newCard.data';
 
-describe('EXIST A MAIN AND COMPACT VIEW OF A FLASH CARD', () => {
+describe('CARDS MAIN VIEW AND COMPACT VIEW', () => {
     before('login as a student', () => {
         LoginPage.login(student);
         browser.pause(500);
     });
 
-    it('should go to `FlashCards` page', () => {
+    it('should open `FlashCards` page', () => {
         FlashCardsPage.open();
-        browser.pause(2000);
         expect(pageTestData.title).equal(FlashCardsPage.h1.getText());
     });
 
-    it('should go to `FlashCards` group page', () => {
+    it('should open a specified `Flash Card`', () => {
         browser.$(groupNameLink).click();
-        browser.pause(2000);
         expect(pageTestData.groupName).equal(FlashCardsPage.h1.getText());
     });
 
-   it ('should redirect to `Main view`', () => {
-       MainAndCompactViewPage.mainView.click();
-       expect(MainAndCompactViewPage.mainViewActive.isExisting()).equal(true)
-   });
+    it ('should redirect to `Main view`', () => {
+        MainAndCompactViewPage.mainView.click();
+        expect(MainAndCompactViewPage.mainViewActive.isExisting()).equal(true)
+    });
+
+    it ('should check that question appears under `Main view`', () => {
+        expect(MainAndCompactViewPage.cardQuestion.isExisting()).equal(true)
+    });
+
+    it ('should check that answer appears under question', () => {
+        expect(MainAndCompactViewPage.cardAnswer.isExisting()).equal(true)
+    });
 
     it ('should redirect to `Compact view`', () => {
         MainAndCompactViewPage.compactView.click();
         expect(MainAndCompactViewPage.compactViewActive.isExisting()).equal(true)
     });
 
-    it ('should check that the text does not appear before hovering over the question', () => {
+    it ('should check that answer does not appear before hovering over the question', () => {
         expect(MainAndCompactViewPage.answerText.isExisting()).equals(false);
     });
 
-    it ('should check that the text appears when hovering over the question', () => {
+    it ('should check that answer appears when hovering over the question', () => {
         MainAndCompactViewPage.questionText.moveTo();
-        browser.pause(100);
         expect(MainAndCompactViewPage.answerText.isExisting()).equals(true);
     });
 
