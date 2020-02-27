6c700a55440266213f9f1de5bf3b3e9fcd43371d
Amdrei Kalianchuk <andrey.kalenchuk@gmail.com>
2020-02-26 23:11:44 -0500

    add coursesChallengeNegativeNoSolution.spec.js add coursesChallengeNegativeWrongSolution.spec.js refactor

--- modules/courses/courses_challenge/coursesChallenge.spec.js
+++ modules/courses/courses_challenge/coursesChallenge.spec.js
@@ -9,6 +9,7 @@ import Notification from '../../_PageObjects/Notification';
 import Menu from '../../_PageObjects/Menu';
 
 describe('ADD A NEW CHALLENGE TO CREATED COURSE ', () => {
+
   before('should login as Admin, create Course, create Challenge', () => {
     LoginPage.login(admin);
     CoursesPage.createNewCourseGlobal;
